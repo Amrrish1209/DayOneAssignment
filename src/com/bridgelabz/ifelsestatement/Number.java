4a9e0aed2a083a64e21985f13163fa6b9594a189
Amrrish1209 <roshanbabubl@gmail.com>
2023-03-17 11:51:31 +0530

    Add:Read a Number 1,10,100,1000 and display unit, ten hundred

--- src/com/bridgelabz/ifelsestatement/Number.java
+++ src/com/bridgelabz/ifelsestatement/Number.java
@@ -3,7 +3,6 @@ package com.bridgelabz.ifelsestatement;
 public class Number {
 
 	public static void main(String[] args) {
-
 		int number = 100;
 		if (number == 1) {
 			System.out.println("One");
