fed6a221efac62ff7f56b7efa3fb7cd0db97fa3c
Amrrish1209 <roshanbabubl@gmail.com>
2023-03-17 15:02:28 +0530

    Add: Java Program to print the elements of an array present on odd position

---
+++ src/com/bridgelabz/arrayhandling/OddPosition.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.arrayhandling;
+
+public class OddPosition {
+
+	public static void main(String[] args) {
+
+		int arr[] = { 1, 2, 3, 4, 5, 6 };
+		for (int i = 0; i < arr.length; i = i + 2) {
+			System.out.println(arr[i]);
+		}
+	}
+}
