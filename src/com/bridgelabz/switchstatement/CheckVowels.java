6aad2f023ce8d960d724c95c6bb569dc8ab5f449
Amrrish1209 <roshanbabubl@gmail.com>
2023-03-17 12:12:11 +0530

    Add: Write a Program to Check Vowel or Consonant

---
+++ src/com/bridgelabz/switchstatement/CheckVowels.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.switchstatement;
+
+public class CheckVowels {
+
+	public static void main(String[] args) {
+
+		String ch = "e";
+
+		switch (ch) {
+		case "a":
+			System.out.println(ch + " is a vowel");
+			break;
+		case "e":
+			System.out.println(ch + " is a vowel");
+			break;
+		case "i":
+			System.out.println(ch + " is a vowel");
+			break;
+		case "o":
+			System.out.println(ch + " is a vowel");
+			break;
+		case "u":
+			System.out.println(ch + " is a vowel");
+			break;
+		default:
+			System.out.println(ch + " is a consonant");
+		}
+	}
+}
