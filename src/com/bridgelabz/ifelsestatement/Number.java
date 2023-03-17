b6072443ae16fc6bf28b109b5e8ab1f503b54cb7
Amrrish1209 <roshanbabubl@gmail.com>
2023-03-17 11:45:18 +0530

    Add:Read a single Digit Number and write in word

---
+++ src/com/bridgelabz/ifelsestatement/Number.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.ifelsestatement;
+
+public class Number {
+
+	public static void main(String[] args) {
+
+		int number = 100;
+		if (number == 1) {
+			System.out.println("One");
+		} else if (number == 10) {
+			System.out.println("Ten");
+		} else if (number == 100) {
+			System.out.println("Hundred");
+		} else if (number == 1000) {
+			System.out.println("Thousand");
+		} else {
+			System.out.println("Invalid number");
+		}
+	}
+}
