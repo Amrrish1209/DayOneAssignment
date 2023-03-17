ad8fab1569b2a9bd796de89a2b080aff68b34da6
Amrrish1209 <roshanbabubl@gmail.com>
2023-03-17 12:13:03 +0530

    Add: Write a Program to print the month name for the given number

---
+++ src/com/bridgelabz/switchstatement/MonthName.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.switchstatement;
+
+public class MonthName {
+
+	public static void main(String[] args) {
+
+		int number = 3;
+
+		switch (number) {
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		case 3:
+			System.out.println("Wednesday");
+			break;
+		case 4:
+			System.out.println("Thursday");
+			break;
+		case 5:
+			System.out.println("Friday");
+			break;
+		case 6:
+			System.out.println("Saturday");
+			break;
+		case 7:
+			System.out.println("Sunday");
+		default:
+			System.out.println("Invalid Number");
+		}
+	}
+}
